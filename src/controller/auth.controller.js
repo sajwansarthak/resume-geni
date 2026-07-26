ae64c7808a85d8115a08e06130e60dcc455cfff9
sajwansarthak <sarthaksajwan25@gmail.com>
2026-07-26 09:11:12 +0530

    Created userlogoutCOntroller and added it to authroutes

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
-const cookie = require("cookie-parser")
+const tokenBlacklistModel = require("../models/blacklist.model")
 
 /**
  * @name registerUserController
@@ -117,8 +117,29 @@ async function loginUserController(req,res){
     })
 }
 
+/**
+ * @name logoutUserController
+ * @description clear token from user cookie and token in the blacklist
+ * @access Public
+ */
+
+async function logoutUserController(req,res){
+    const token = req.cookie.token;
+
+    if(token){
+        await tokenBlacklistModel.create({ token })
+    }
+
+    res.clearCookie("token")
+
+    res.status(200).json({
+        message: "User logged out successfully"
+    })
+}
+
 //Right now exporting an empty object
 module.exports = {
     registerUserController,
+    loginUserController,
     loginUserController
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,17 +8,24 @@ const authController = require("../controller/auth.controller")
 // const authRouter = Router()
 
 /**
- * @route api/auth/register
+ * @route POST api/auth/register
  * @description Register new user
  * @access public
  */
 authRouter.post("/register",authController.registerUserController)
 
 /**
- * @route api/auth/login
+ * @route POST api/auth/login
  * @description login user with email and password
  * @access Public
  */
 authRouter.post("/login",authController.loginUserController)
+
+/**
+ * @route GET api/auth/logout
+ * @description clear token from user cookie and token in the blacklist
+ * @access Public
+ */
+authRouter.get("/logout",authController.logoutUserController)
 
 module.exports = authRouter
