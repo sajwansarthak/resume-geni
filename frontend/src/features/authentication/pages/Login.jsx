b31868fb3f9848fda687a357897828a1fbac60ee
sajwansarthak <sarthaksajwan25@gmail.com>
2026-07-26 21:22:29 +0530

    Created the ui for login page added style folder with button.scss and imported it in style.scss

--- frontend/src/features/authentication/pages/Login.jsx
+++ frontend/src/features/authentication/pages/Login.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import "../auth.form.scss"
 
 const Login = () =>{
+
+    //stop the reload
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
     return (
-        <>Login</>
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
+
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="Enter email address" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="text" id="password" name="password" placeholder="Enter the password" />
+                    </div>
+
+                    <button className="button primary-button">Login</button>
+                </form>
+            </div>
+        </main>
     )
 }
 
