3526c5f09eaa7a9d149fd4e00472b117771b59bb
sajwansarthak <sarthaksajwan25@gmail.com>
2026-07-27 03:07:56 +0530

    Added cors package to tell the origin of frontend and seted credentials:true for cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors") 
 
 const app = express()
-const cookieParser = require("cookie-parser") 
+
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 //Require all the routes here
 const authRouter = require("./routes/auth.routes")
