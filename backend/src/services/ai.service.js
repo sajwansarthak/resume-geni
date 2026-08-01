b9d05da60219564620c689080413cc3e2ee23108
sajwansarthak <sarthaksajwan25@gmail.com>
2026-08-01 08:18:45 +0530

    Changed the ai-model

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -49,7 +49,7 @@ async function generateInterviewReport ({ resume, selfDescription, jobDescriptio
 
 
     const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
+        model: "gemini-3.5-flash",
         contents: prompt,
         config:{
             responseMimeType: "application/json",
